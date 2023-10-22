bf325c30cc5165f43d2816ff4ff7a4c5a2fc7fe6
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2023-10-21 21:17:01 -0300

    add method to get lyrics

---
+++ src/get_lyrics.py
@@ -0,0 +1,19 @@
+import os
+import lyricsgenius as lg
+
+# pip3 install lyricsgenius
+
+def get_lyrics(music: str, artist: str):
+    token = os.getenv('GENIUS_ACCESS_TOKEN')
+    genius = lg.Genius(token)
+    song = genius.search_song(music, artist)
+
+    return song.lyrics
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+    lyrics = get_lyrics('Free Bird', 'Lynyrd Skynyrd')
+    print(f'type: {type(lyrics)}')
+    print(lyrics)
+

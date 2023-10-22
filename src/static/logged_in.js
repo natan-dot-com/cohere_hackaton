f11e02f5c50fccba364dd15bf1e9d57a96f23a81
Gabriel Dertoni <gab.dertoni@gmail.com>
2023-10-22 01:43:41 -0300

    add basic styles

---
+++ src/static/logged_in.js
@@ -0,0 +1,47 @@
+
+const BACKEND = "http://localhost:8888/callback/";
+const CLIENT_ID = "4b01eb5194cd4aa6afb6aa9e31eca0f4";
+
+// Local Storage key for storing spotify info
+const SPOTIFY_INFO_KEY = "SPOTIFY_INFO"
+
+const generateBtn = document.getElementById("generate");
+const generateBtnContent = document.getElementById("generate-btn-content");
+const loading = document.getElementById("loading");
+
+const info = localStorage.getItem(SPOTIFY_INFO_KEY);
+if (!info) {
+    window.location.replace("/")
+} else {
+    var { accessToken, profile } = JSON.parse(info);
+}
+
+async function generateSong() {
+    startLoading();
+
+    const song = await fetch(`/api/generate_song`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            profileId: profile.id,
+            accessToken,
+        })
+    });
+
+    stopLoading();
+}
+
+function startLoading() {
+    generateBtn.disabled = true;
+    generateBtnContent.style.display = "none";
+    loading.style.display = "block";
+}
+
+
+function stopLoading() {
+    generateBtn.disabled = false;
+    generateBtnContent.style.display = "block";
+    loading.style.display = "none";
+}
